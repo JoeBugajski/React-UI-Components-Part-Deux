49779a58a47281bd8967dd2c35a5646d82d5cfaf
Joe Bugajski <yogurtmystery@gmail.com>
2019-02-11 18:55:35 -0600

    added header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className='headerTitle'>
+      <h2>Lambda School</h2>
+      <p>@Lambda School - 26 jan</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
